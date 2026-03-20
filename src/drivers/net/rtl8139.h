d00e62b27dbd50da2eaa4504652156be630f802f
ewasion137 <formanban94@gmail.com>
2026-03-20 20:06:25 +0200

    att to get time from internet

--- src/drivers/net/rtl8139.h
+++ src/drivers/net/rtl8139.h
@@ -8,6 +8,7 @@ void rtl8139_send_packet(void* data, uint32_t len);
 void send_ethernet_frame(uint8_t* dest_mac, uint16_t ethertype, uint8_t* payload, uint32_t payload_len);
 void send_arp_request(uint32_t target_ip);
 void send_ntp_request();
+uint16_t ip_checksum(void* vdata, uint32_t length);
 void rtl8139_receive();
 
 #endif
