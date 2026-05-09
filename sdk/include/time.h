0c876c7737a7c673a277acf9cce4ba42bb0bbe53
ewasion137 <formanban94@gmail.com>
2026-05-09 11:42:46 +0300

    Added time.h

---
+++ sdk/include/time.h
@@ -0,0 +1,26 @@
+#ifndef _TIME_H
+#define _TIME_H
+
+#include <stddef.h>
+
+typedef long time_t;
+typedef long clock_t;
+
+struct tm {
+  int tm_sec;
+  int tm_min;
+  int tm_hour;
+  int tm_mday;
+  int tm_mon;
+  int tm_year;
+  int tm_wday;
+  int tm_yday;
+  int tm_isdst;
+};
+
+time_t time(time_t *t);
+clock_t clock(void);
+struct tm *gmtime(const time_t *timep);
+struct tm *localtime(const time_t *timep);
+
+#endif
