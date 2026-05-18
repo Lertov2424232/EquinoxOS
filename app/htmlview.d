a3050965969c001e9549291d45666883a0f04165
ewasion137 <formanban94@gmail.com>
2026-05-18 20:30:33 +0300

    Many updates

---
+++ app/htmlview.d
@@ -0,0 +1,6 @@
+app/htmlview.o: app/htmlview.c sdk/include/eid.h sdk/include/equos.h \
+ sdk/include/stdio.h sdk/include/string.h
+sdk/include/eid.h:
+sdk/include/equos.h:
+sdk/include/stdio.h:
+sdk/include/string.h:
---
+++ src/system/cpu.h
@@ -0,0 +1,34 @@
+#ifndef CPU_H
+#define CPU_H
+
+#include <stdint.h>
+
+static inline void stac() {
+    __asm__ volatile("stac" ::: "cc");
+}
+
+static inline void clac() {
+    __asm__ volatile("clac" ::: "cc");
+}
+
+static inline uint64_t read_cr0() {
+    uint64_t val;
+    __asm__ volatile("mov %%cr0, %0" : "=r"(val));
+    return val;
+}
+
+static inline void write_cr0(uint64_t val) {
+    __asm__ volatile("mov %0, %%cr0" : : "r"(val));
+}
+
+static inline uint64_t read_cr4() {
+    uint64_t val;
+    __asm__ volatile("mov %%cr4, %0" : "=r"(val));
+    return val;
+}
+
+static inline void write_cr4(uint64_t val) {
+    __asm__ volatile("mov %0, %%cr4" : : "r"(val));
+}
+
+#endif
