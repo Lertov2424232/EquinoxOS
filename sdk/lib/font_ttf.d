33019a3fd0f7a4936b1fbf39938ee9b7e24d13bc
ewasion137 <formanban94@gmail.com>
2026-05-18 21:45:19 +0300

    COLORS IN TERMINAL

--- sdk/lib/font_ttf.d
+++
@@ -1,9 +0,0 @@
-sdk/lib/font_ttf.o: sdk/lib/font_ttf.c sdk/include/eid.h \
- sdk/include/math.h sdk/include/stdio.h sdk/include/stdlib.h \
- sdk/include/string.h sdk/include/stb_truetype.h
-sdk/include/eid.h:
-sdk/include/math.h:
-sdk/include/stdio.h:
-sdk/include/stdlib.h:
-sdk/include/string.h:
-sdk/include/stb_truetype.h:
