5ed5b7e21900a6e819b0417ebb5dc5fe4449bc75
ewasion137 <formanban94@gmail.com>
2026-05-19 21:00:49 +0300

    Updates on keyboard...

--- src/drivers/keyboard/keyboard.h
+++ src/drivers/keyboard/keyboard.h
@@ -4,6 +4,10 @@
 #include <stdint.h>
 
 void keyboard_callback();
+void keyboard_push_string(const char *s);
 char get_ascii_char(uint8_t scancode);
+void keyboard_push(uint8_t scancode);
+uint8_t keyboard_pop();
+
 
 #endif
